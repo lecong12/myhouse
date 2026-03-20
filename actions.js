68d506048ce1c7607ff34e16f74553a8c244c4dd
Lê Công <lecong12@gmail.com>
2026-03-20 08:50:28 +0700

    cập nhật

--- actions.js
+++ actions.js
@@ -1,22 +1,22 @@
 'use server';
 
-import dbConnect from '@/lib/mongodb';
-import Transaction from '@/models/Transaction';
+import connectDB from '../lib/mongodb';
+import Transaction from '../models/Transaction';
 import { revalidatePath } from 'next/cache';
 
 export async function addTransaction(formData) {
-  await dbConnect();
-  
-  const data = {
-    title: formData.get('title'),
-    amount: Number(formData.get('amount')),
-    type: formData.get('type'),
-    category: "Xây dựng", // Có thể mở rộng sau
-    date: new Date(),
-  };
-
-  await Transaction.create(data);
-  
-  // Yêu cầu Next.js làm mới dữ liệu trên trang chủ
-  revalidatePath('/');
+  try {
+    await connectDB();
+    const data = {
+      title: formData.get('title'),
+      amount: Number(formData.get('amount')),
+      type: formData.get('type'),
+      category: "Xây dựng",
+      date: new Date(),
+    };
+    await Transaction.create(data);
+    revalidatePath('/');
+  } catch (error) {
+    console.error("Lỗi Action:", error);
+  }
 }
